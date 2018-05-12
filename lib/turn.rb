fa258c0db8ae054effa4ce1021614038b88d2928
Josh Cesana <joshuacesana@gmail.com>
2018-05-12 22:25:47 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -12,3 +12,21 @@ def display_board(board)
   puts row5
 end
 
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    false
+  elsif board[index] == "X" || board[index] == "O"
+    true
+  end
+end
+
+
+def valid_move?(board, index)
+  
+  if !position_taken?(board, index) && index.between?(0, 8)
+    true 
+  else 
+    false
+  end
+  
+end
