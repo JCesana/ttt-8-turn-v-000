3e279ce913e617c4e94b351a3885f2466488d39d
Josh Cesana <joshuacesana@gmail.com>
2018-05-12 22:23:46 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,14 @@
+def display_board(board)
+  row1 = " #{board[0]} | #{board[1]} | #{board[2]} "
+  row2 = "-----------"
+  row3 = " #{board[3]} | #{board[4]} | #{board[5]} "
+  row4 = "-----------"
+  row5 = " #{board[6]} | #{board[7]} | #{board[8]} "
+  
+  puts row1
+  puts row2
+  puts row3
+  puts row4
+  puts row5
+end
+
